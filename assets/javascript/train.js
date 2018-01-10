e10faa14d983828ecb1a0929f7a856d145cd8adc
JoJo Zheng^C <jojozheng1216@hotmail.com>
2018-01-09 18:28:00 -0700

    added html

---
+++ assets/javascript/train.js
@@ -0,0 +1,16 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAa0vcyOIxbZLFPFJ9v9yW-Enhrr8RjYNU",
+    authDomain: "train-firebase-179ce.firebaseapp.com",
+    databaseURL: "https://train-firebase-179ce.firebaseio.com",
+    projectId: "train-firebase-179ce",
+    storageBucket: "",
+    messagingSenderId: "187295448968"
+  };
+  firebase.initializeApp(config);
+
+  var database= firebase.database();
+
+  console.log('hello');
+
+  
