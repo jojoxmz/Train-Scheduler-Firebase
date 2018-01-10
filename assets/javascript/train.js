7ad823281b47439d21f02f5be6911625385be89e
JoJo Zheng^C <jojozheng1216@hotmail.com>
2018-01-09 19:41:33 -0700

    added JS file

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -13,4 +13,32 @@
 
   console.log('hello');
 
-  
+
+
+
+  $('#submit').on('click', function(e){
+	e.preventDefault();
+
+	var trainName= $('#trainNameId').val().trim();
+	var trainDestination= $('#destinationId').val().trim();
+	var trainTime= $('#trainTimeId').val().trim();
+	var trainFrequency= parseInt($('#frequencyId').val().trim());
+
+	console.log(trainName);
+	console.log(trainDestination);
+	console.log(trainTime);
+	console.log(trainFrequency);
+
+	var newTrain= {
+		name: trainName,
+		destination: trainDestination,
+		time: trainTime,
+		frequency: trainFrequency
+	};
+
+	console.log(newTrain);
+
+	database.ref().push(newTrain);
+
+	$('.form-group > input').val("");
+});
