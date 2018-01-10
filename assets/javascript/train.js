d9e190d4bbbbb45888231ade7db3dac53c7b3b0f
JoJo Zheng^C <jojozheng1216@hotmail.com>
2018-01-09 21:37:26 -0700

    finished js

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -7,39 +7,48 @@
     storageBucket: "",
     messagingSenderId: "187295448968"
   };
+
   firebase.initializeApp(config);
 
   var database= firebase.database();
-
-  console.log('hello');
 
 	database.ref().on("child_added", function(snapshot){
 		console.log(snapshot.val());
 		console.log(snapshot.val().name);
 		console.log(snapshot.val().destination);
-		console.log(snapshot.val().time);
+		console.log(snapshot.val().firstTime);
 		console.log(snapshot.val().frequency);
 
+		var newestFirstTime= (snapshot.val().firstTime);
+		var newestFrequency= (snapshot.val().frequency);
 
+		var firstTimeConverted = moment(newestFirstTime, "hh:mm").subtract(1, "years");
+			console.log(firstTimeConverted);
+		
+		var currentTime= moment();
+			console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
-	// $('#tableBody').append('<tr><td>' + snapshot.val().name + '</td>' + 
-	// 		'<td>' + snapshot.val().name + '</td>' + 
-	// 		'<td>' + snapshot.val().destination + '</td>' +
-	// 		'<td>' + snapshot.val().time + '</td>') +
-	// 		'<td>' + snapshot.val().frequency + '</td>';
+		var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+			console.log("DIFFERENCE IN TIME: " + diffTime);
+
+		var tRemainder = diffTime % newestFrequency;
+    		console.log(tRemainder);
+
+		var tMinutesTillTrain = newestFrequency - tRemainder;
+    		console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+		var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+   			console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
 	var newRow= `<tr>
 					<td>${snapshot.val().name}</td>
 					<td>${snapshot.val().destination}</td>
 					<td>${snapshot.val().frequency}</td>
-					<td>${snapshot.val().time}</td>
+					<td>${nextTrain}</td>
+					<td>${tMinutesTillTrain}</td>
 				</tr>`;
 
 	$('#tableBody').append(newRow);
-
-
-
-
 
 	}, function(errorObject) {
  	console.log("Failed: " + errorObject.code);
@@ -53,18 +62,17 @@
 
 	var trainName= $('#trainNameId').val().trim();
 	var trainDestination= $('#destinationId').val().trim();
-	var trainTime= $('#trainTimeId').val().trim();
+	var firstTrainTime= $('#trainTimeId').val().trim();
 	var trainFrequency= parseInt($('#frequencyId').val().trim());
 
 	console.log(trainName);
 	console.log(trainDestination);
-	console.log(trainTime);
 	console.log(trainFrequency);
 
 	var newTrain= {
 		name: trainName,
 		destination: trainDestination,
-		time: trainTime,
+		firstTime: firstTrainTime,
 		frequency: trainFrequency
 	};
 
